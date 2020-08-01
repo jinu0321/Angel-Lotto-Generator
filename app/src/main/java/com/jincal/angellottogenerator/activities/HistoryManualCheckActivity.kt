e16327eef48437576b1b597237db767d83dbb7f9
KJW <57553647+nogosan@users.noreply.github.com>
2020-08-01 14:48:05 +0900

    history manual check done

--- app/src/main/java/com/jincal/angellottogenerator/activities/HistoryManualCheckActivity.kt
+++ app/src/main/java/com/jincal/angellottogenerator/activities/HistoryManualCheckActivity.kt
@@ -1,10 +1,18 @@
 package com.jincal.angellottogenerator.activities
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jincal.angellottogenerator.R
+import com.jincal.angellottogenerator.classes.LottoRealmObject
+import com.jincal.angellottogenerator.objects.RealmConfigurationSupplier
+import com.jincal.angellottogenerator.objects.SelectedBallHolder
 import kotlinx.android.synthetic.main.activity_history_manual_check.*
 import com.jincal.angellottogenerator.objects.ViewController
+import io.realm.Realm
+import io.realm.kotlin.where
+import kotlinx.android.synthetic.main.activity_generator.*
+import org.jetbrains.anko.textColor
 
 class HistoryManualCheckActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,5 +71,51 @@ class HistoryManualCheckActivity : AppCompatActivity() {
             manualEmptyBall4
         )
         ViewController.setLottoBallViewsSize(balls)
+
+        val selectedBallList = mutableListOf<Int>()
+
+        fun checkResult() {
+            if (selectedBallList.size == 6) {
+                val realm = Realm.getInstance(RealmConfigurationSupplier.lottoApiConfiguration)
+                val temp = realm.where<LottoRealmObject>().findAll().map { it.toLotto() }
+
+                var equalCount = 0
+                manualFirstPrizeExistTextVIewTmp.textColor = Color.BLACK
+                manualFirstPrizeExistTextVIewTmp.text = "1등 당첨 이력이 없는 번호입니다."
+                for (lotto in temp) {
+                    for (i in 0..5) {
+                        if (lotto.ballNumberList[i] == selectedBallList[i]) equalCount++
+                    }
+                    if (equalCount == 6) {
+                        manualFirstPrizeExistTextVIewTmp.textColor = Color.RED
+                        manualFirstPrizeExistTextVIewTmp.text = "${lotto.episode}회 1등 번호입니다."
+                        break
+                    }
+                    equalCount = 0
+                }
+            } else {
+                manualFirstPrizeExistTextVIewTmp.textColor = Color.GRAY
+                manualFirstPrizeExistTextVIewTmp.text = "번호를 6개 선택해주세요."
+            }
+        }
+        checkResult()
+
+        fun toggle(ballNumber: Int) {
+            if (!selectedBallList.contains(ballNumber)) {
+                selectedBallList.add(ballNumber)
+                ViewController.setLottoBallView(balls[ballNumber - 1], ballNumber)
+            } else if (selectedBallList.contains(ballNumber)) {
+                selectedBallList.remove(ballNumber)
+                balls[ballNumber - 1].setBackgroundResource(R.drawable.background_ball_zero)
+            }
+            selectedBallList.sort()
+            checkResult()
+        }
+
+        for (ballIndex in 0..44) {
+            balls[ballIndex].setOnClickListener {
+                toggle(ballIndex + 1)
+            }
+        }
     }
 }
